8ce3126e9854116d821bef68f2856bcbdc665717
egor_thinkpad <froggamestudio7@gmail.com>
2024-09-05 16:22:13 +0300

    func resreve is changed, add new func: push_back(probably UB)

--- C++/my_vector/my_vector.cpp
+++ C++/my_vector/my_vector.cpp
@@ -1,35 +1,101 @@
 #include <iostream>
 
-template<typename T>
-class vector {
+// TODO: UB - heap-buffer-overflow
+
+template <typename T>
+class vector
+{
 public:
-    vector() : arr(nullptr) , sz(0) , cap(0) {}
-    vector(const vector& other) : arr(new T[other.sz]) , sz(other.sz) , cap(other.cap) {
-        for(size_t index = 0;index < other.sz;++index) {
-            this->arr[index] = other.arr[index];
-        }
-    }
-    vector& operator=(const vector& other);
-    ~vector();
-private:
-    
-    //memory reserve
-    void reserve(size_t sz) {
-        try{
-            arr = reinterpret_cast<T*>(new char[sizeof(T)  * sz]); // allocates sizeof(T) * N bytes 
-        } catch(std :: bad_alloc& ex) {
-            std :: cout << ex.what() << std :: endl;
-        }
-        for(size_t index = 0;index < sz;++index) {
-            new(arr + index)T(); // default inicialization
+    vector() : arr(nullptr), sz(0), cap(0) {}
+    vector(size_t sz, T value = T()) : arr(new T[sz]), sz(sz), cap(sz * sizeof(T))
+    {
+        for (size_t index = 0; index < sz; ++index)
+        {
+            arr[index] = value;
         }
     }
 
-    T* arr;
+    vector(const vector &other) : arr(new T[other.sz]), sz(other.sz), cap(other.cap)
+    {
+        for (size_t index = 0; index < other.sz; ++index)
+        {
+            this->arr[index] = other.arr[index];
+        }
+    }
+
+    void push_back(T value)
+    {
+        T *_newarr = resrerve(sz + 1);
+        for (size_t index = 0; index < sz + 1; ++index)
+        {
+            new (_newarr + index) T(arr[index]);
+        }
+        for (size_t index = 0; index < sz; ++index)
+        {
+            (arr + index)->~T();
+        }
+        new (_newarr + sz) T(value);
+        delete[] reinterpret_cast<char *>(arr);
+        arr = _newarr;
+        sz += 1;
+        cap += sizeof(T);
+    }
+
+    vector &operator=(const vector &other)
+    {
+        vector copy = other;
+        swap(copy);
+        return *this;
+    }
+
+    ~vector()
+    {
+        delete[] reinterpret_cast<char *>(arr);
+    }
+
+private:
+    // memory reserve
+    T *resrerve(size_t sz)
+    {
+        T *_newarr = nullptr;
+        try
+        {
+            _newarr = reinterpret_cast<T *>(new char[sz * sizeof(T)]);
+        }
+        catch (std ::bad_alloc &exc)
+        {
+            std ::cout << exc.what() << std ::endl;
+            std ::abort();
+        }
+        return _newarr;
+    }
+    friend std ::ostream &operator<<(std ::ostream &os, const vector<T> &vec)
+    {
+        for (size_t index = 0; index < vec.sz; ++index)
+        {
+            os << vec.arr[index] << ' ';
+        }
+        os << std ::endl;
+        return os;
+    }
+
+    void swap(vector &other)
+    {
+        std ::swap(arr, other.arr);
+        std ::swap(sz, other.sz);
+        std ::swap(cap, other.cap);
+    }
+
+    T *arr;
     size_t sz;
     size_t cap;
 };
 
-int main() {
+int main()
+{
+    vector<int> v(5, 4);
+    std ::cout << v;
+    v.push_back(3);
+    std ::cout << v;
     return 0;
 }
